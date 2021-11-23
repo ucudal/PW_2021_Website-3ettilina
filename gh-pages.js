9c50b138babb8e649f5b8f86557323069498b401
Bettina Carrizo <betticarrizo@gmail.com>
2021-11-23 19:19:44 -0300

    Update gh-pages file

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public',
     {
-        branch: 'gh-pages',
+        branch: 'main',
         repo: 'https://github.com/ucudal/PW_2021_Website-3ettilina.git', 
         user: {
             name: 'Bettina Carrizo',
